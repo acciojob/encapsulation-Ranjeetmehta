4a74ca841bd1fdd9249023518f48c6aad223aaf5
ranjeetmehta <ranjeetkumarmehta14418@gmail.com>
2023-05-26 10:29:05 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+
+        RWOnly obj=new RWOnly();
+        // RWOnly obj1=new RWOnly();
+
+
+
+        obj.setName("Ranjeet");
+
+
+        String name=obj.getName();
+
+
+        System.out.println(name);
+
+
+
+
+    }
+
 }
